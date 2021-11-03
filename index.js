2a354228b9c985b3760044460d2656fd4afda1b9
rameez03 <codewithrameez@gmail.com>
2021-11-03 22:43:51 +0530

    added routers

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const bodyparser = require('body-parser');
+const flash = require("connect-flash")
 const session = require('express-session');
 const path = require('path');
+const port = process.env.PORT || 3000
+
+// Models
 const Buyer = require('./models/buyer')
 const Seller = require('./models/seller')
 const Product = require('./models/product')
+
+// Routes
+const mainRoutes = require("./routes/main")
+const buyerRoutes = require("./routes/buyer")
+const sellerRoutes = require("./routes/seller")
+const adminRoutes = require("./routes/admin")
+
+var Publishable_Key = 'pk_test_51JrkwZSGTTdwKf5g4xzsgRumwqOEvzQd0zlLDzT5JvcJtuEhx7XEEdVZElAXYmIhTsK37fvdP8KHHUcVEOFgx7Eu00rE7oFu1Q'
+var Secret_Key = 'sk_test_51JrkwZSGTTdwKf5gbqJzAxZV1YQtFlFSjfB2kFd2URZM7n9AmRkyiwBUnnKQ1tGxh6AS2CAG609jK8SWJg1oNHmh00BfVoMlLZ'
+
+const stripe = require('stripe')(Secret_Key)
 
 mongoose.connect('mongodb://localhost:27017/shophop', { useNewUrlParser: true })
 	.then(()=>{
@@ -17,159 +33,25 @@ mongoose.connect('mongodb://localhost:27017/shophop', { useNewUrlParser: true })
 		console.log(err)
 	})
 
+// View engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
+app.use(flash());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static( "public" ));
 app.use(session({ secret: 'thisaverygoodsecret'} ))
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
 
-const buyerLogin = (req, res, next) => {
-	if(!req.session.buyer_id){
-		return res.redirect('/login')
-	}
-	next();
-}
-
-const sellerLogin = (req, res, next) => {
-	if(!req.session.seller_id){
-		return res.redirect('/sellerlogin')
-	}
-	next();
-}
-
-app.get('/', (req, res) => {
-	res.render('landing')
-})
-
-app.get('/aboutus', (req, res) => {
-	res.render('about')
-})
-
-app.get('/signup', (req, res) => {
-	res.render('buyersignup')
-})
-
-app.post('/signup', async(req, res) => {
-	const { username, email, password } = req.body;
-	const buyer = new Buyer({ username, email, password })
-	await buyer.save();
-	res.redirect('/login')
-})
-
-app.get('/login', (req, res) => {
-	res.render('buyerlogin')
-	req.session.destroy()
-})
-
-app.post('/login', async(req, res) => {
-	const { email, password } = req.body;
-	const foundBuyer = await Buyer.validateBuyer(email, password)
-	if(foundBuyer){
-		req.session.buyer_id = foundBuyer._id
-		res.redirect('/home')
-	}
-	else{
-		res.redirect('/login')
-	}
-})
-
-app.get('/home', buyerLogin, async(req, res) => {
-	const buyer_id = req.session.buyer_id
-	const buyer = await Buyer.findOne({ _id: buyer_id })
-	const username = buyer.username
-	Product.find({}, function(err, allProducts){
-        if(err){
-           console.log(err);
-        } else 
-           res.render('buyerhome', { username , products: allProducts})
-        })
-})
-
-app.get('/cart', buyerLogin, (req, res) => {
-	Buyer.findOne({ _id: req.session.buyer_id }, function(err, buyer){
-        if(err){
-           console.log(err);
-        } else {
-           Product.find({ _id: buyer.cart_items }, function(err, items){
-           if(err){
-		   console.log(err);
-		} else {
-		   res.render('cart', { buyer, items })
-		}
-           })
-        }
-        })
-})
-
-app.post('/addtocart/:id', buyerLogin, (req, res) => {
-	Buyer.updateOne({ _id: req.session.buyer_id }, { $push: {cart_items: req.params.id }}, function(err, buyer){
-        if(err){
-           console.log(err);
-        } else {
-           res.redirect('/home')
-        }
-        })
-})
-
-app.post('/logout', (req, res) => {
-	req.session.destroy()
-	res.redirect('/login')
-})
-
-app.get('/sellersignup', (req, res) => {
-	res.render('sellersignup')
-})
-
-app.post('/sellersignup', async(req, res) => {
-	const { username, email, password, phone, city } = req.body;
-	const seller = new Seller({ username, email, password, phone, city })
-	await seller.save();
-	res.redirect('/sellerlogin')
-})
-
-app.get('/sellerlogin', (req, res) => {
-	res.render('sellerlogin')
-	req.session.destroy()
-})
-
-app.post('/sellerlogin', async(req, res) => {
-	const { email, password } = req.body;
-	const foundSeller = await Seller.validateSeller(email, password)
-	if(foundSeller){
-		req.session.seller_id = foundSeller._id
-		res.redirect('/sellerhome')
-	}
-	else{
-		res.redirect('/sellerlogin')
-	}
-})
-
-app.post('/sellerlogout', (req, res) => {
-	req.session.destroy()
-	res.redirect('/sellerlogin')
-})
-
-app.get('/sellerhome', sellerLogin, async(req, res) => {
-	const seller_id = req.session.seller_id
-	const seller = await Seller.findOne({ _id: seller_id })
-	const username = seller.username
-	res.render('sellerhome', { username })
-})
-
-app.get('/listitem', sellerLogin, (req, res) => {
-	res.render('listitem')
-})
-
-app.post('/listitem', sellerLogin, (req, res) => {
-	res.send(req.body)
-})
+app.use("/", mainRoutes);
+app.use("/buyer", buyerRoutes);
+app.use("/seller", sellerRoutes);
 
 app.get('*', (req, res) => {
 	res.render('undefined')
 })
 
-const port = 3001;
 app.listen(port, () => {
 	console.log(`Server running on Port ${port}`)
 })
